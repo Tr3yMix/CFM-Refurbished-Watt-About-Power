5cc614ec9c1801360e8a88f3ac8d350595b1930b
Tr3yMix <treyhoke04@gmail.com>
2025-08-27 16:38:03 -0500

    moved init inside common setup

--- neoforge/src/main/java/dev/tr3ymix/cfm_wap/neoforge/CFM_WAP_NeoForge.java
+++ neoforge/src/main/java/dev/tr3ymix/cfm_wap/neoforge/CFM_WAP_NeoForge.java
@@ -1,11 +1,9 @@
 package dev.tr3ymix.cfm_wap.neoforge;
 
-import com.mrcrayfish.furniture.refurbished.core.ModCreativeTabs;
 import dev.tr3ymix.cfm_wap.block.CircuitBreakerBlock;
 import dev.tr3ymix.cfm_wap.client.gui.screen.CircuitBreakerScreen;
 import dev.tr3ymix.cfm_wap.client.renderer.CircuitBreakerBlockEntityRenderer;
 import dev.tr3ymix.cfm_wap.registry.ModBlockEntities;
-import dev.tr3ymix.cfm_wap.registry.ModBlocks;
 import dev.tr3ymix.cfm_wap.registry.ModMenuTypes;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
 import net.minecraft.world.level.block.state.BlockState;
@@ -16,22 +14,23 @@ import net.neoforged.fml.common.Mod;
 
 import dev.tr3ymix.cfm_wap.CFM_WAP;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import net.neoforged.neoforge.energy.IEnergyStorage;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 
 @Mod(CFM_WAP.MOD_ID)
 public final class CFM_WAP_NeoForge {
     public CFM_WAP_NeoForge(IEventBus modEventBus) {
-        CFM_WAP.init();
 
+        modEventBus.addListener(this::onCommonSetup);
         modEventBus.addListener(this::onRegisterCapabilities);
     }
 
-
-
+    private void onCommonSetup(FMLCommonSetupEvent event){
+        event.enqueueWork(CFM_WAP::init);
+    }
 
     private void onRegisterCapabilities(RegisterCapabilitiesEvent event){
         event.registerBlockEntity(Capabilities.EnergyStorage.BLOCK, ModBlockEntities.CIRCUIT_BREAKER.get(),
